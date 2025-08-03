08b61d3928d5d1dcb5548f786a2a0c3be1b1b18c
Morgan Hondros <morganhondros@gmail.com>
2025-08-03 12:15:44 -0400

    first commit

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -2,13 +2,15 @@ require("dotenv").config()
 const { ethers } = require("ethers")
 
 // Setup connection
+
 const URL = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
 const provider = new ethers.JsonRpcProvider(URL)
 
 const ADDRESS = "0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e"
+// const ADDRESS = "0xB3ABc4b2475382930Ea5681B8AFC820018D60590"
 
 async function main() {
-  // Get balance
+  // Get balancegit 
   const balance = await provider.getBalance(ADDRESS)
 
   // Log balance
